ccad499472764fb2aea26b174c2d074559856805
fernandocto.scz <fernandocto.scz@gmail.com>
2019-10-08 11:25:29 -0400

    Initial scheme

---
+++ server.js
@@ -0,0 +1,48 @@
+'use strict'
+
+const debug = require('debug')('informatica:api')
+const morgan = require('morgan')
+const http = require('http')
+const express = require('express')
+const cors = require('cors')
+const compression = require('compression')
+const chalk = require('chalk')
+const nconf = require('nconf')
+const settings = nconf.file({ file: 'config/config.json' })
+
+const port = settings.get('settings').port || 80
+const format = settings.get('settings').morgan || 'tiny'
+
+const app = express()
+const server = http.createServer(app)
+app.use(morgan(format))
+app.use(cors())
+app.use(compression())
+app.use((err, req, res, next) => {
+  debug(`Error: ${err.message}`)
+
+  if (err.message.match(/not found/)) {
+    return res.status(404).send({ error: err.message })
+  }
+
+  res.status(500).send({ error: {
+    name: err.name,
+    message: err.message
+  }})
+})
+
+function handleFatalError (err) {
+  console.error(`${chalk.red('[fatal error]')} ${err.message}`)
+  console.error(err.stack)
+  process.exit(1)
+}
+
+if (!module.parent) {
+  process.on('uncaughtException', handleFatalError)
+  process.on('unhandledRejection', handleFatalError)
+
+  server.listen(port, () => {
+    debug('connecting...')
+    console.log(`${chalk.green('[SERVER SUCCESSFUL]')} Server listening on port ${port}`)
+  })
+}
