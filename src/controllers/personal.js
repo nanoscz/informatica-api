5a8a09a6889d21e898b9a021fd33e37b939a922b
fernandocto.scz <fernandocto.scz@gmail.com>
2019-10-15 15:46:03 -0400

    Add query and range

--- src/controllers/personal.js
+++ src/controllers/personal.js
@@ -1,11 +1,41 @@
 'use strict'
 
 const Personal = require('../models').personal
+const Sequelize = require('sequelize')
+const Op = Sequelize.Op
 
 class PersonalController {
+  
   findAll (req, res, next) {
-    Personal.findAll()
-      .then(personals => res.json(personals))
+    const condition = {}
+    const fields= ['nom', 'app', 'apm']
+    if (Object.keys(req.query).length) {
+      const search = req.query.search
+      condition.where = {[Op.or]: []}
+      for (const field of fields) {
+        condition.where[Op.or].push({
+          [field]: {
+            [Op.like]: `%${search}%`
+          }
+        })
+      }
+      if (req.query.range) {
+        const range = req.query.range.split('-')
+        condition.offset = parseInt(range[0], 10)
+        condition.limit = parseInt(range[1], 10)
+      }
+    }
+    Personal.count(condition)
+      .then(count => {
+        Personal.findAll(condition)
+          .then(personals =>{
+            const range = `${req.query.range}/${count}`
+            res.status(206)
+            res.append('Content-Range', range)
+            res.json({ personals, count })
+          })
+          .catch(err => next(err))
+      })
       .catch(err => next(err))
   }
 
