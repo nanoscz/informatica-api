9571582e1af11b01ef5d0a11ec5fca728fc2b480
unknown <fernandocto.scz@gmail.com>
2019-10-27 06:25:15 -0400

    Add fields id

--- src/models/solicitud.js
+++ src/models/solicitud.js
@@ -2,6 +2,7 @@
 
 module.exports = (sequelize, DataTypes) => {
   const solicitud = sequelize.define('solicitud', {
+    id: DataTypes.INTEGER,
     ruta: DataTypes.INTEGER,
     fecha: DataTypes.DATE,
     referencia: DataTypes.TEXT,
